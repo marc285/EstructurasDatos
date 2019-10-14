dd34ba0202a91e0b414a87e4589208f25876e660
marc285 <marc285ee@gmail.com>
2019-10-14 08:54:25 +0200

    Segunda versión. Todos los tests funcionan correctamente

--- src/test/java/TestQueues.java
+++ src/test/java/TestQueues.java
@@ -7,7 +7,6 @@ public class TestQueues {
 
     @Before
     public void SetUp() throws ColaLlenaException { //Común a todos los @Test's que se hagan
-
         prueba = new QueueImpl<String>(5);
         prueba.push("Hola");
         prueba.push("Que");
@@ -16,7 +15,8 @@ public class TestQueues {
 
     @Test
     public void pushTest() throws ColaLlenaException {
-        Assert.assertEquals(3, prueba.size()); //Comprobamos que los 3 push se han hecho correctamente
+        //Comprobamos que los 3 push se han hecho correctamente
+        Assert.assertEquals(3, prueba.size());
 
         //Comprobamos que al hacer un nuevo push sigue funcionando
         prueba.push("Te");
@@ -31,12 +31,25 @@ public class TestQueues {
 
     @Test(expected = ColaLlenaException.class)
     public void colallenaTest() throws ColaLlenaException{
-        Assert.assertEquals(3, prueba.size()); //Comprobamos que los 3 push se han hecho correctamente
+        //Comprobamos que los 3 push se han hecho correctamente
+        Assert.assertEquals(3, prueba.size());
 
         //Comprobamos que al introducir más de 5 objetos la cola desborda
         prueba.push("Te");
         prueba.push("Va");
         Assert.assertEquals(5, prueba.size());
         prueba.push("Bien");
+    }
+
+    @Test(expected = ColaVaciaException.class)
+    public void colavaciaTest() throws ColaVaciaException{
+        //Comprobamos que los pops se hacen correctamente
+        Assert.assertEquals("Hola", prueba.pop());
+        Assert.assertEquals("Que", prueba.pop());
+        Assert.assertEquals("Tal", prueba.pop());
+        Assert.assertEquals(0, prueba.size());
+
+        //Comprobamos que al hacer un pop con la cola vacía salta la excepción
+        prueba.pop();
     }
 }
